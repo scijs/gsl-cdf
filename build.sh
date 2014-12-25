4de18ceeddad59f7d1e296a44900b57467fce9bd
Planeshifter <pburckhardt@outlook.com>
2014-12-25 13:35:53 +0100

    fix build process

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ emmake make
 
 cd $DIR
 
-emcc -O3 gsl-js/cdf/*.o gsl-js/err/*.o gsl-js/randist/*.o gsl-js/specfunc/*.o gsl-js/gsl/*.h --bind cdf.cpp -I./ --memory-init-file 0 -o cdf.js
+emcc -O3 gsl-js/cdf/*.o gsl-js/err/*.o gsl-js/randist/*.o gsl-js/specfunc/*.o gsl-js/sys/*.o --bind cdf.cpp -I./gsl-js --memory-init-file 0 -o cdf.js
 
 echo "JS build finished"
